193aba94e6923245438dddc89af0350012d56fda
Mintimate <mintimate215@gmail.com>
2025-06-04 16:23:29 +0800

    refactor: 重构boot2rescue.sh脚本，将功能模块化为函数并优化输出格式

--- boot2rescue.sh
+++ boot2rescue.sh
@@ -11,6 +11,11 @@ GREEN='\033[0;32m'  # 绿色
 YELLOW='\033[0;33m' # 黄色
 NC='\033[0m'        # 重置颜色
 
+# 设置镜像源
+repo="https://mirrors.tuna.tsinghua.edu.cn/alpine/edge"
+# 指定内核URL，从网络直接加载，防止占用本地硬盘，导致无法操作
+modl="$repo/releases/x86_64/netboot/modloop-virt"
+
 printMintimate() {
     echo -e "${GREEN}
 _____________________________________________________________
@@ -37,28 +42,27 @@ _____________________________________________________________${NC}"
     https://www.mintimate.cn
     
     Mintimate's Bilibili:
-    https://space.bilibili.com/355567627${NC}"
+    https://space.bilibili.com/355567627
+_____________________________________________________________${NC}"
 }
 
-printMintimate
+mkdirNetboot() {
+  # 创建netboot目录并进入
+  echo -e "${GREEN} 正在创建 netboot 目录并进入 ${NC}"
+  mkdir -p /netboot && cd /netboot || exit
+}
 
-# 创建netboot目录并进入
-echo -e "${GREEN} 正在创建 netboot 目录并进入 ${NC}"
-mkdir -p /netboot && cd /netboot || exit
+downloadNetBoot() {
+  # 下载 netboot 文件
+  echo -e "${GREEN} 正在下载 netboot 文件 ${NC}"
+  wget -q "$repo/releases/x86_64/netboot/vmlinuz-virt" || exit
+  wget -q "$repo/releases/x86_64/netboot/initramfs-virt" || exit
+}
 
-# 设置镜像源
-repo="https://mirrors.tuna.tsinghua.edu.cn/alpine/edge"
-
-# 下载必要文件
-echo -e "${GREEN} 正在下载必要文件 ${NC}"
-# 静默下载
-wget -q "$repo/releases/x86_64/netboot/vmlinuz-virt" || exit
-wget -q "$repo/releases/x86_64/netboot/initramfs-virt" || exit
-modl="$repo/releases/x86_64/netboot/modloop-virt"
-
-# 添加GRUB启动项
-echo -e "${GREEN} 正在添加 GRUB 启动项 ${NC}"
-cat >> /etc/grub.d/40_custom <<EOF
+modifyGrub() {
+    # 添加GRUB启动项
+    echo -e "${GREEN} 正在添加 GRUB 启动项 ${NC}"
+    cat >> /etc/grub.d/40_custom <<EOF
 menuentry "Alpine Linux Minimal" {
     search --set=root --file /netboot/vmlinuz-virt
     linux /netboot/vmlinuz-virt console=tty0 console=ttyS0,115200 ip=dhcp modloop=$modl alpine_repo=$repo/main/
@@ -66,19 +70,27 @@ menuentry "Alpine Linux Minimal" {
 }
 EOF
 
-# 修改默认启动项
-echo -e "${GREEN} 是否修改默认启动项 ${NC}"
-echo -e "${YELLOW} 输入y后回车=>修改；其他键后回车=>不修改 ${NC}"
-echo -e "${YELLOW} 修改后，grub 默认首选启动替换为 Alpine Linux Minimal ${NC}"
-read temp
-if [ ${temp} = "y" ];then
-    echo -e "${GREEN} 修改默认启动项 ${NC}"
-    sed -i 's/GRUB_DEFAULT=0/GRUB_DEFAULT="Alpine Linux Minimal"/' /etc/default/grub
-else
-    echo -e "${GREEN} 不修改默认启动项 ${NC}"
-fi
+    # 修改默认启动项
+    echo -e "${GREEN} 是否修改默认启动项 ${NC}"
+    echo -e "${YELLOW} 输入y后回车=>修改；其他键后回车=>不修改 ${NC}"
+    echo -e "${YELLOW} 修改后，grub 默认首选启动替换为 Alpine Linux Minimal ${NC}"
+    read temp
+    if [ ${temp} = "y" ]; then
+        echo -e "${GREEN} 修改默认启动项 ${NC}"
+        sed -i 's/GRUB_DEFAULT=0/GRUB_DEFAULT="Alpine Linux Minimal"/' /etc/default/grub
+    else
+        echo -e "${GREEN} 不修改默认启动项 ${NC}"
+    fi
+}
 
-# 更新 GRUB 配置
-echo -e "${GREEN} 更新 GRUB 配置 ${NC}"
-update-grub
-echo -e "${GREEN} Done ${NC}"
+updateGrub() {
+    # 更新 GRUB 配置
+    echo -e "${GREEN} 更新 GRUB 配置 ${NC}"
+    update-grub
+}
+
+
+printMintimate
+mkdirNetboot
+downloadNetBoot
+modifyGrub
